7044b391348496effd71cda32d4f925abfda3edd
rohnshrma <rohn11shrma@gmail.com>
2025-09-15 20:00:03 +0530

    cfs and loops

--- javascript classes/cfs.js
+++ javascript classes/cfs.js
@@ -40,15 +40,95 @@
 // else if (today === 6) console.log("Saturday");
 // else console.log(`${today} is not a valid day`);
 
-var age = parseInt(prompt("Enter your age : "));
+// var age = parseInt(prompt("Enter your age : "));
 
-if (age >= 18) {
-  var yourname = prompt("Enter your name : ").toLowerCase();
-  if (yourname.length > 3 && yourname.startsWith("s")) {
-    console.log(`Welcome to party, ${yourname}`);
-  } else {
-    console.log("Not Invited");
-  }
-} else {
-  console.log("Not Eligible To Party!!");
-}
+// if (age >= 18) {
+//   var yourname = prompt("Enter your name : ").toLowerCase();
+//   if (yourname.length > 3 && yourname.startsWith("s")) {
+//     console.log(`Welcome to party, ${yourname}`);
+//   } else {
+//     console.log("Not Invited");
+//   }
+// } else {
+//   console.log("Not Eligible To Party!!");
+// }
+
+// var today = parseInt(prompt("Enter the day today : ", "0-6"));
+// switch (today) {
+//   case 0:
+//     console.log("Sunday");
+//     break;
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   default:
+//     console.log("INVALID DAY");
+//     break;
+// }
+
+// var age = parseInt(prompt("Enter your age : "));
+// switch (age) {
+//   case 0:
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     console.log("FREE");
+//     break;
+//   case 5:
+//   case 6:
+//   case 7:
+//   case 8:
+//   case 9:
+//   case 10:
+//     console.log(100);
+//     break;
+//   case 11:
+//   case 12:
+//   case 13:
+//   case 14:
+//   case 15:
+//     console.log(500);
+//     break;
+//   default:
+//     console.log(1500);
+//     break;
+// }
+
+// var marks = parseInt(prompt("Enter your marks"));
+// switch (true) {
+//   case marks > 90 && marks <= 100:
+//     console.log("A");
+//     break;
+//   case marks > 80 && marks <= 90:
+//     console.log("B");
+//     break;
+//   case marks > 70 && marks <= 80:
+//     console.log("C");
+//     break;
+//   case marks > 60 && marks <= 70:
+//     console.log("D");
+//     break;
+
+//   case marks >= 0 && marks <= 60:
+//     console.log("F");
+//     break;
+//   default:
+//     console.log("Invalid marks");
+//     break;
+// }
---
+++ javascript classes/loop.js
@@ -0,0 +1,62 @@
+// fixed iteration : the loop runs a fixed number of times
+
+// for (initialization ; condition ; update){
+// code to be executed
+// }
+
+// for (var water = 0; water < 1000; water += 100) {
+//   console.log(water);
+// }
+
+// for (var i = 11; i <= 20; i += 1) {
+//   console.log(i, "john doe");
+// }
+
+// for (var i = 1; i <= 5; i += 1) {
+//   console.log(i);
+// }
+
+// for (var i = 5; i >= 1; i -= 1) {
+//   console.log(i);
+// }
+
+// for (var i = 2; i <= 100; i += 2) {
+//   console.log(i);
+// }
+
+// for (var i = 1; i <= 100; i += 1) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} Even`);
+//   } else {
+//     console.log(`${i} Odd`);
+//   }
+// }
+
+// for (var i = 1; i <= 100; i += 1) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+// 2 x 4 = 8
+// 2 x 5 = 10
+// 2 x 6 = 12
+// 2 x 7 = 14
+// 2 x 8 = 16
+// 2 x 9 = 18
+// 2 x 10 = 20
+
+// var n = parseInt(prompt("Enter a number : "));
+
+// for (var i = 1; i <= 10; i += 1) {
+//   console.log(`${n} x ${i} = ${i * n}`);
+// }
